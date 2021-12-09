a4279a35ed05feba983baedc0c1f1ad902ffb437
rjiang <rich.jiang2@gmail.com>
2021-12-08 23:22:19 -0500

    garbadge

--- app/src/main/java/com/example/optionsalpha/SearchActivity.kt
+++ app/src/main/java/com/example/optionsalpha/SearchActivity.kt
@@ -1,19 +1,15 @@
 package com.example.optionsalpha
 
-import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.MenuItem
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_search)
+
         val nav_bar = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         nav_bar.setOnNavigationItemSelectedListener { item ->
             when(item.itemId){
@@ -38,16 +34,4 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-//
-//    override fun onCreateView(
-//        parent: View?,
-//        name: String,
-//        context: Context,
-//        attrs: AttributeSet
-//    ): View? {
-//
-//
-//        return super.onCreateView(parent, name, context, attrs)
-//    }
-
 }
