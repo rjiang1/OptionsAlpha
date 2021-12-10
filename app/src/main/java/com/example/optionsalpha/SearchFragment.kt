2452bcf5b9717fc2df36255d7ed7efbaed8028db
rjiang <rich.jiang2@gmail.com>
2021-12-09 20:52:53 -0500

    figured out the sync issue with polygon

--- app/src/main/java/com/example/optionsalpha/SearchFragment.kt
+++ app/src/main/java/com/example/optionsalpha/SearchFragment.kt
@@ -8,9 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
